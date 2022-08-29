aedfe9806aa6412ada9b2960504c6b8e7d649691
Viktor Kleen <viktor@kleen.org>
2022-08-29 16:20:14 +0000

    Add a ZornIdentity decoding fuzzer

--- fuzz/src/bin/fuzz_identity.rs
+++ fuzz/src/bin/fuzz_identity.rs
@@ -1,9 +1,10 @@
 use afl::fuzz;
+use zorn_core::identity::ZornIdentity;
 
 fn main() {
     fuzz!(|data: &[u8]| {
         if let Ok(s) = std::str::from_utf8(data) {
-            let _ = url::Url::parse(&s);
+            let _ = s.parse::<ZornIdentity>();
         }
     });
 }
